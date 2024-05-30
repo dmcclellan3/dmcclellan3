fa583f0ead1481ae14d67def1c22058fad7f664b
dmcclellan3 <wdmcclellan3@gmail.com>
2024-05-29 21:35:36 -0400

    contact card update

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom"
+import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './Contact.css';
 
 
 
@@ -21,29 +23,27 @@ function Contact(){
                     </Nav>
                 </Container>
             </Navbar>
-        <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="card" style={{ width: "22rem" }}>
-              <img
-                src='blog photo.jpeg'
-                className="card-img-top"
-                alt="contact card photo"
-              />
-              <div className="card-body">
-                <h4>Duke McClellan</h4>
-                <div className="card-text" id="icons">
-                  <img src="./Contact-Card-Icons/Email.svg"alt="gmail" />
-                  <img src="./Contact-Card-Icons/Github.svg" alt="github" />
-                  <img src="./Contact-Card-Icons/Instagram.svg" alt="instagram" />
-                  <img src="./Contact-Card-Icons/Facebook.svg" alt="facebook" />
-                  <img src="https://icons8.com/icon/114445/linkedin-circled" alt="LinkedIn" />
-                </div> 
-              </div>
+            <div className="contact-card">
+            <div className="profile-section">
+                <img src="https://media.licdn.com/dms/image/D4E03AQEbwJ59Zqv6TQ/profile-displayphoto-shrink_800_800/0/1715535669511?e=1722470400&v=beta&t=skBM7QmwIonzp_Gx3wGXbqXWBPnuCnQnBxJxRCKfLMM" alt="Profile" className="profile-pic" />
+                <div className="profile-info">
+                    <h2 className="name">Duke McClellan</h2>
+                    <p className="title">Full Stack Web Developer</p>
+                    {/* <button className="contact-button">Contact me</button> */}
+                </div>
             </div>
-          </div>
+            <div className="social-icons">
+                <a href="mailto:wdmcclellan3@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <img src="https://img.icons8.com/color/48/000000/gmail-new.png" alt="Gmail" />
+                </a>
+                <a href="https://github.com/dmcclellan3?tab=repositories" target="_blank" rel="noopener noreferrer">
+                    <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" />
+                </a>
+                <a href="https://www.linkedin.com/in/duke-mcclellan-3234b274/" target="_blank" rel="noopener noreferrer">
+                    <img src="https://content.linkedin.com/content/dam/me/brand/en-us/brand-home/logos/In-Blue-Logo.png" alt="LinkedIn" />
+                </a>
+            </div>
         </div>
-       </div>
       </div>
       
     )
